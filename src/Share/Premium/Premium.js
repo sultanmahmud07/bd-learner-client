d645e8f4b4511ff7f38d2e8ba64a3b87a85932ac
sultanmahmud07 <asadkhan01862@gmail.com>
2022-10-27 23:04:15 +0600

    premium section contains added

--- src/Share/Premium/Premium.js
+++ src/Share/Premium/Premium.js
@@ -1,9 +1,115 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const Premium = () => {
   return (
     <div>
-      <h2 className='text-center text-warning'>This is Premium!!</h2>
+      <h2 className='text-center text-warning p-4'>This is Premium!!</h2>
+
+      <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-4'>
+
+        <div className='col'>
+          <Card border="primary">
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Primary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className='col'>
+          <Card border="secondary">
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Secondary Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className='col'>
+          <Card border="success">
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Success Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className='col'>
+          <Card border="danger">
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Danger Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className='col'>
+          <Card border="warning" >
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Warning Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className='col'>
+
+          <Card border="info" >
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Info Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+        </div>
+        <div className='col'>
+          <Card border="dark">
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Dark Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+        </div>
+        <div className='col'>
+          <Card border="dark" >
+            <Card.Header>Header</Card.Header>
+            <Card.Body>
+              <Card.Title>Dark Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <br />
+        </div>
+
+      </div>
     </div>
   );
 };
