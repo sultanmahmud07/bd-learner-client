6c0b2225450c4fadaadb29438efef6c8b6b2d258
sultanmahmud07 <asadkhan01862@gmail.com>
2022-10-27 13:36:12 +0600

    home page banner img added

--- src/Share/Home/Home.js
+++ src/Share/Home/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+import Img from '../../Assat/meeting-tipps-erfolgreich-1200x630.jpg';
+import './Home.css';
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
-      <h1>This is home page</h1>
+      <div className="home-page">
+        <div className="container d-flex flex-column flex-sm-column flex-xl-row">
+          <div className="left-img w-100 w-lg-50">
+            <img className='img-fluid' src={Img} alt="" />
+          </div>
+          <div className="right-info w-100 w-lg-50 mt-lg-5">
+            <h2 className='home-title'> <span className='title'>Welcome to our school,</span> We are always ready to help you!</h2>
+            <p className='home-p'>You will get various opportunities in our school. You will get many types of questions and answers here, you will get a chance to take the exam on any subject. So stay with us and learn.</p>
+            
+            <div className="btn-container ">
+              <button className='home-btn mt-2 ' id='home-btn1'><Link to='/course'>Course</Link></button>
+              <button className='home-btn ms-2 mt-2' id='home-btn2'><Link to='/blog'>Blog</Link></button>
+              
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
